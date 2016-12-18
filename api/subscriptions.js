16883397f00b207ab31d4017fc6f01e4a0de3f4f
Benjamin Chodoroff <bc@thermitic.net>
2016-12-18 15:16:13 -0500

    a more meaningful example of setupFunctions

--- api/subscriptions.js
+++ api/subscriptions.js
@@ -2,14 +2,24 @@ import { PubSub, SubscriptionManager } from 'graphql-subscriptions';
 import schema from './schema';
 
 const pubsub = new PubSub();
+
 const subscriptionManager = new SubscriptionManager({
-        schema,
-        pubsub,
-        setupFunctions: {
-            commentAdded: (options, args) => ({
-            commentAdded: comment => comment.repoName === args.repoFullName,
+  schema,
+  pubsub,
+  setupFunctions: {
+    countUpdated: () => ({
+      countUpdated: {
+        filter(count) {
+          // This is a silly example of how the subscription filter works.
+          // Clients will not see updates when the counter hits 13.
+          // A more meaningful example can be found at
+          // https://dev-blog.apollodata.com/graphql-subscriptions-in-apollo-client-9a2457f015fb#4af9
+          const shouldit = count.amount !== 13;
+          return shouldit;
+        },
+      },
     }),
-    },
-    });
+  },
+});
 
 export { subscriptionManager, pubsub };

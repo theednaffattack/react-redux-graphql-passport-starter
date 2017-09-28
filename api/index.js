df2acd34843c922db3fa35f89537453820976321
theednaffattack <eddienaff@gmail.com>
2017-09-28 07:51:12 -0700

    Add graphiql endpoint with JWT Auth header to API PORT

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import { graphqlExpress } from 'graphql-server-express';
-import bodyParser from 'body-parser';
-import { createServer } from 'http';
-import { SubscriptionServer } from 'subscriptions-transport-ws';
-import passport from 'passport';
+import dotenv from "dotenv";
+import express from "express";
+import { graphqlExpress, graphiqlExpress } from "graphql-server-express";
+import bodyParser from "body-parser";
+import { createServer } from "http";
+import { SubscriptionServer } from "subscriptions-transport-ws";
+import passport from "passport";
 
-import { subscriptionManager } from './subscriptions';
-import schema from './schema';
-import * as CounterService from './services/countService';
-import setupLocalLogin from './localLogin';
+import { subscriptionManager } from "./subscriptions";
+import schema from "./schema";
+import * as CounterService from "./services/countService";
+import setupLocalLogin from "./localLogin";
 
 dotenv.config();
 
@@ -22,26 +22,41 @@ app.use(bodyParser.json());
 
 setupLocalLogin(app);
 
-app.use('/graphql', passport.authenticate('jwt', { session: false }), graphqlExpress((req) => {
-  const query = req.query.query || req.body.query;
-  if (query && query.length > 2000) {
-    throw new Error('Query too large.');
-  }
+app.use(
+  "/graphql",
+  passport.authenticate("jwt", { session: false }),
+  graphqlExpress(req => {
+    const query = req.query.query || req.body.query;
+    if (query && query.length > 2000) {
+      throw new Error("Query too large.");
+    }
 
-  return {
-    schema,
-    context: {
-      user: req.user,
-      counterService: CounterService,
-    },
-  };
-}));
+    return {
+      schema,
+      context: {
+        user: req.user,
+        counterService: CounterService
+      }
+    };
+  })
+);
 
-app.use(express.static('dist'));
+app.use(
+  "/graphiql",
+  graphiqlExpress({
+    endpointURL: "/graphql",
+    passHeader: `'Authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjAsImlhdCI6MTUwNTg2Njk2OTcyNywidXNlcm5hbWUiOiJ1c2VyMSJ9.BaQ9CH1RRSUU-clGpNcJPNnbpPfqEa8ozflUutPCq3o'`
+  })
+);
 
-app.listen(API_PORT, () => console.log( // eslint-disable-line no-console
-  `API Server is now running on http://localhost:${API_PORT}`,
-));
+app.use(express.static("dist"));
+
+app.listen(API_PORT, () =>
+  console.log(
+    // eslint-disable-line no-console
+    `API Server is now running on http://localhost:${API_PORT}`
+  )
+);
 
 const WS_PORT = process.env.WS_PORT;
 
@@ -51,14 +66,17 @@ const websocketServer = createServer((request, response) => {
   response.end();
 });
 
-websocketServer.listen(WS_PORT, () => console.log( // eslint-disable-line no-console
-  `Websocket Server is now running on http://localhost:${WS_PORT}`,
-));
+websocketServer.listen(WS_PORT, () =>
+  console.log(
+    // eslint-disable-line no-console
+    `Websocket Server is now running on http://localhost:${WS_PORT}`
+  )
+);
 
 // eslint-disable-next-line
 new SubscriptionServer(
   {
-    subscriptionManager,
+    subscriptionManager
   },
-  websocketServer,
+  websocketServer
 );
